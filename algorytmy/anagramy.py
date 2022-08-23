88d43664bc05de9e4f07b8b53e497ea3683695ce
ThomasKarpinski <tomaszkarpinski204@gmail.com>
2022-08-23 21:44:58 +0200

    anagramy

---
+++ algorytmy/anagramy.py
@@ -0,0 +1,31 @@
+def sortuj(s):
+    """
+    :param s: słowo
+    :return: posortowane alfabetycznie słowo
+    """
+    n = len(s)
+    s = list(s)
+    for j in range(n - 1, 0, -1):
+        for i in range(j):
+            if s[i] > s[i + 1]:
+                s[i], s[i + 1] = s[i + 1], s[i]
+    s = ''.join(s)
+    return s
+
+
+def anagramy():
+    """
+    :return: sprawdza czy podane słowa są anagramem
+    """
+    s1 = input("podaj pierwsze słowo: ")
+    s2 = input("podaj drugie słowo: ")
+    if len(s1) == len(s2):
+        if sortuj(s1) == sortuj(s2):
+            print(True)
+        else:
+            print(False)
+    else:
+        print(False)
+
+
+anagramy()
