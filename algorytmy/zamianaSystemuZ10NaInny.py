978b191ae698f3d92b76f2b099ac407157ae37b9
ThomasKarpinski <tomaszkarpinski204@gmail.com>
2022-09-06 23:17:27 +0200

    zadania

---
+++ algorytmy/zamianaSystemuZ10NaInny.py
@@ -0,0 +1,21 @@
+"""Program zamienia liczbę z systemu 10
+ na dowolny inny system o podstawie p"""
+
+
+def zamien10_p(liczba, p):
+    """
+    :param liczba w systemie 10
+    :param p: podstawa dowolnego innego systemu
+    :return: przekonwerterowana liczba
+    """
+    w = ''
+    while liczba > 0:
+        if liczba % p >= 10:
+            w = chr(liczba % p + 55) + w
+        else:
+            w = str(liczba % p) + w
+        liczba //= p
+    return w
+
+
+print(zamien10_p(135, 2))
---
+++ matura2009_PP_Python/zad2_cenyWSystemachDziesiętnymIDwójkowym.py
@@ -0,0 +1,15 @@
+def zad_b(s):
+    """
+    :param s: liczba w systemie dwójkowym
+    :return: liczba w systemie dziesiętnym
+    """
+    wynik = 0
+    k = 0.25
+    for i in range(len(s) - 1, -1, -1):
+        if s[i] != ',':
+            wynik += k*(ord(s[i]) - ord('0'))
+            k *= 2
+    return wynik
+
+
+print(zad_b('10000111,01'))
