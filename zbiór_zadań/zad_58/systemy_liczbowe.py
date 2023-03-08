c2e856f402f6053259c3684fc2519731f44b567f
ThomasKarpinski <tomaszkarpinski204@gmail.com>
2023-03-08 23:18:54 +0100

    zad58

--- zbiór_zadań/zad_58/systemy_liczbowe.py
+++ zbiór_zadań/zad_58/systemy_liczbowe.py
@@ -1,3 +1,4 @@
+import math
 files = ['dane_systemy1.txt', 'dane_systemy2.txt', 'dane_systemy3.txt']
 
 with open(files[0]) as dane1, open(files[1]) as dane2, open(files[2]) as dane3:
@@ -10,32 +11,16 @@ temp2 = [int(i[1]) for i in S2]
 temp3 = [int(i[1]) for i in S3]
 
 """58.1"""
-
-
-def horner(a, p):
-    h = 0
-    sign = 1
-    if a[0] == '-':
-        sign = -1
-        a = a[1:]
-    for i in a:
-        if ord(i) >= 65:
-            h = h * p + ord(i) - 55
-        else:
-            h = h * p + int(i)
-    return h*sign
-
-
 min1 = min(temp1)
-min2 = bin(horner(str(min(temp2)), 4))[3:]
-min3 = bin(horner(str(min(temp3)), 8))[3:]
+min2 = bin(int(str(min(temp2)), 4))[3:]
+min3 = bin(int(str(min(temp3)), 8))[3:]
 
 print(min1, '-' + min2, '-' + min3)
 
 """58.2"""
-hour1 = [horner(i[0], 2) for i in S1]
-hour2 = [horner(i[0], 4) for i in S2]
-hour3 = [horner(i[0], 8) for i in S3]
+hour1 = [int(i[0], 2) for i in S1]
+hour2 = [int(i[0], 4) for i in S2]
+hour3 = [int(i[0], 8) for i in S3]
 
 licznik = 0
 for h1, h2, h3 in zip(hour1, hour2, hour3):
@@ -44,3 +29,30 @@ for h1, h2, h3 in zip(hour1, hour2, hour3):
 print(licznik)
 
 """58.3"""
+maks1, maks2, maks3 = temp1[0], temp2[0], temp3[0]
+licz = 1
+rekord = bool
+for i in range(len(temp1)):
+    rekord = False
+    if maks1 < temp1[i]:
+        rekord = True
+        maks1 = temp1[i]
+    if maks2 < temp2[i]:
+        rekord = True
+        maks2 = temp2[i]
+    if maks3 < temp3[i]:
+        rekord = True
+        maks3 = temp3[i]
+    if rekord:
+        licz += 1
+
+print(licz)
+
+"""58.4"""
+maxi = []
+
+for i in range(len(temp1)):
+    for j in range(i + 1, len(temp1)):
+        r_ij = (int(str(temp1[i]), 2) - int(str(temp1[j]), 2)) ** 2
+        maxi.append(math.ceil(r_ij / abs(i - j)))
+print(max(maxi))
